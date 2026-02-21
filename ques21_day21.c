54953d96cae9be5a55e7e37c9fd6128f64d62758
Shivang Joshi <shivangjoshi499@gmail.com>
2026-02-21 23:04:58 +0530

    Add files via upload

---
+++ ques21_day21.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n <= 0) return 0;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = createNode(value);
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print the linked list
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
